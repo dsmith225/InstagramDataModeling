9fa63c730c1b01183bed1614159740744d13fdcc
dsmith225 <douglaseugenesmithjr@gmail.com>
2022-12-17 00:56:49 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -5,7 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
-
+import enum
+from sqlalchemy import Integer, Enum
 Base = declarative_base()
 
 class Person(Base):
@@ -29,5 +30,44 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True)
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class MediaType(enum.Enum):
+    png = "png"
+    jpg = "jpg"
+    gif = "gif"
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    type = Column(Enum(MediaType))
+    url = Column(String(250), nullable=False)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    comment_text = Column(String(250), nullable=False)
+
+    
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
